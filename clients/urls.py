684b16deec3b18fd623234a7dfc5b73a8314e174
majksoon <michalmekal@icloud.com>
2024-11-24 23:48:20 +0100

    Dodalem panel rejestracji

--- clients/urls.py
+++ clients/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="client_home"),
-    path("login/", views.client_login, name="client_login"),
-    path("home/", views.client_home, name="client_home"),
+    path("", views.home, name="home"),  # Strona główna aplikacji
+    path("login/", views.client_login, name="client_login"),  # Logowanie klienta
+    path("home/", views.client_home, name="client_home"),  # Strona sukcesu po zalogowaniu
+    path("register/", views.ClientRegistrationView.as_view(), name="client_register"),  # Rejestracja klienta
 ]
