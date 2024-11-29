62d84891dbc05204487d0fd57e653a9512261ed2
R3VANEK <janek2000-2000@wp.pl>
2024-11-29 12:29:37 +0100

    styling the login and register forms

--- clients/views.py
+++ clients/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-from .forms import ClientLoginForm, ClientRegistrationForm  # Dodaj nowy formularz rejestracji
+from .forms import (
+    ClientLoginForm,
+    ClientRegistrationForm,
+)
 from django.http import HttpResponse
 
 
 # Widok logowania klienta
 def client_login(request):
+    is_error = False
     if request.method == "POST":
         form = ClientLoginForm(request.POST)
         if form.is_valid():
@@ -17,26 +21,22 @@ def client_login(request):
             if client is not None:
                 login(request, client)
                 return redirect("client_home")
+        is_error = True
     else:
         form = ClientLoginForm()
 
-    return render(request, "login.html", {"form": form})
+    return render(request, "login.html", {"form": form, "is_error": is_error})
 
 
-# Widok strony sukcesu po zalogowaniu
 def client_home(request):
     return render(request, "success.html")
 
 
-# Widok strony głównej
 def home(request):
     return HttpResponse("Witaj na stronie głównej aplikacji Klienci!")
 
 
-# Widok rejestracji klienta (oparty na klasie)
-
-
 class ClientRegistrationView(CreateView):
-    template_name = 'clients/register.html'  # Ścieżka do szablonu rejestracji
-    form_class = ClientRegistrationForm       # Formularz rejestracji klienta
-    success_url = reverse_lazy('client_login')  # Przekierowanie na stronę logowania
+    template_name = "register.html"
+    form_class = ClientRegistrationForm
+    success_url = reverse_lazy("home")
