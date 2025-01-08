d07a711510784e1e08df58205e7d11c895858963
Mateusz Lysak <matlysak@gmail.com>
2025-01-08 20:34:43 +0100

    To wspólna praca z Michem. Cały 1 punkt prac

--- drivers/urls.py
+++ drivers/urls.py
@@ -1,19 +1,26 @@
 from django.urls import path
-from . import views
-from django.contrib.auth.decorators import login_required
-
+from .views import (
+    driver_login,
+    DriverRegistrationView,
+    driver_vehicles_manage,
+    get_vehicles_endpoint,
+    get_vehicle_endpoint,
+    edit_vehicle_endpoint,
+    add_vehicle_endpoint,
+    delete_vehicle_endpoint,
+    driver_courses_manage,
+    driver_rides_manage,
+)
 
 urlpatterns = [
-    path("vehicles/", login_required(views.driver_vehicles_manage), name="driver_home"),
-    path("vehicles/delete", views.delete_vehicle_endpoint, name="vehicle_delete"),
-    path("vehicles/get", views.get_vehicles_endpoint, name="vehicle_get"),
-    path("vehicles/get/single", views.get_vehicle_endpoint, name="vehicle_get_single"),
-    path("vehicles/edit", views.edit_vehicle_endpoint, name="vehicles_edit"),
-    path("vehicles/add", views.add_vehicle_ednpoint, name="vehicle_add"),
-    path(
-        "courses/", login_required(views.driver_courses_manage), name="driver_courses"
-    ),
-    path("rides/", login_required(views.driver_rides_manage), name="driver_rides"),
-    path("login/", views.driver_login, name="driver_login"),
-    path("register/", views.DriverRegistrationView.as_view(), name="driver_register"),
+    path('login/', driver_login, name='driver_login'),
+    path('register/', DriverRegistrationView.as_view(), name='driver_register'),
+    path('vehicles/', driver_vehicles_manage, name='driver_vehicles_manage'),
+    path('vehicles/get/', get_vehicles_endpoint, name='get_vehicles'),
+    path('vehicles/get/single/', get_vehicle_endpoint, name='get_vehicle'),
+    path('vehicles/edit/', edit_vehicle_endpoint, name='edit_vehicle'),
+    path('vehicles/add/', add_vehicle_endpoint, name='add_vehicle'),
+    path('vehicles/delete/', delete_vehicle_endpoint, name='delete_vehicle'),
+    path('courses/', driver_courses_manage, name='driver_courses_manage'),
+    path('rides/', driver_rides_manage, name='driver_rides_manage'),
 ]
